8d4919afe88b7f7edd03ab72a05a1e6f3ebff2a4
Grant Smits <47378538+Enchae@users.noreply.github.com>
2026-07-26 07:16:01 -0500

    Start button sound

--- Assets/Scripts/MainMenu/MenuClickableButtons.cs
+++ Assets/Scripts/MainMenu/MenuClickableButtons.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Audio;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class MenuClickableButtons : MonoBehaviour, ICursorClickable
 {
     public UnityEvent OnClicked;
+    [SerializeField] private AudioResource startSound;
 
-    public void CursorClick() => OnClicked?.Invoke();
+    public void CursorClick()
+    {
+        SoundManager.PlaySound(startSound, volume: 1f);
+        OnClicked?.Invoke();
+    }
 }
