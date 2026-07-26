55a54cad94aefbc2865aa8195723d3a8cb8361c3
Grant Smits <47378538+Enchae@users.noreply.github.com>
2026-07-25 21:22:18 -0500

    Timer visual

--- Assets/Scripts/PuzzleLogic/LevelTimer.cs
+++ Assets/Scripts/PuzzleLogic/LevelTimer.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class LevelTimer : MonoBehaviour
 {
-    [SerializeField] private float timeLimit = 10f;
+    public float timeLimit = 25f;
 
     [SerializeField] private PuzzleLogicController controller;
 
     [SerializeField] private float loseDelay = 1f;
 
+    [Header("Visuals")]
+    [SerializeField] private SpriteRenderer timeBar;
+    [SerializeField] private SpriteRenderer stopwatch;
+    [SerializeField] private List<Sprite> stopwatchFrames;
+    [SerializeField] private float stopwatchAnimSpeed = 1.5f;
+    [SerializeField] private float timeBarWidthMax = 8f;
+    [SerializeField] private float timeBarSliceWidth = 14f;
+
     public float TimeRemaining { get; private set; }
     private bool ended;
+    private float timeBarWidth = 0f;
+    private Vector2 timeBarDefaultSize = new Vector2(0f, 0f);
+    private Transform stopwatchTransform;
+    private float stopwatchFrameTime = 0f;
+    private int stopwatchFrameIndex = 0;
+
+    private const float pixel_size = 0.0625f;
+
+    private void Awake()
+    {
+        timeBarDefaultSize = timeBar.size;
+        stopwatchTransform = stopwatch.gameObject.transform;
+    }
 
     private void Start()
     {
         TimeRemaining = timeLimit;
+        timeBarWidth = timeBarWidthMax;
 
         if (controller == null)
             controller = FindAnyObjectByType<PuzzleLogicController>();
@@ -26,6 +49,33 @@ public class LevelTimer : MonoBehaviour
             return;
 
         TimeRemaining -= Time.deltaTime;
+
+        
+        var ratio = TimeRemaining / timeLimit;
+
+        var tickSpeed = stopwatchAnimSpeed;
+        if (ratio < 0.35f)
+        {
+            tickSpeed = stopwatchAnimSpeed * 0.4f;
+        }
+        else if (ratio < 0.15f)
+        {
+            tickSpeed = stopwatchAnimSpeed * 0.15f;
+        }
+
+        timeBar.size = new Vector2(timeBarWidthMax * ratio, timeBarDefaultSize.y);
+        stopwatchTransform.localPosition = new Vector3(ratio * (timeBarWidthMax) + (pixel_size * 4f), 0f, 0f);
+
+        stopwatchFrameTime += Time.deltaTime;
+        if (stopwatchFrameTime > tickSpeed)
+        {
+            stopwatchFrameTime -= tickSpeed;
+            stopwatchFrameIndex++;
+            if (stopwatchFrameIndex > 1){
+                stopwatchFrameIndex = 0;
+            }
+            stopwatch.sprite = stopwatchFrames[stopwatchFrameIndex];
+        }
 
         if (TimeRemaining <= 0f)
             StartCoroutine(LevelLose());
